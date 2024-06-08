522ffcbfaed2915c4bbe8b14dedb0c0cefeda7b7
Tech3 <Tech3@imperiorailing.com>
2024-06-08 11:59:37 +0530

    removing the error

--- src/components/TextComponent/TextComponent.tsx
+++ src/components/TextComponent/TextComponent.tsx
@@ -1,72 +1,72 @@
 import React, { useEffect, useRef, useState } from "react";
-import { motion, useScroll, useTransform, inView } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import { createGroups } from "../../utils";
 
 interface TextWrapperProps {
-	className?: string;
-	children: React.ReactNode;
+  className?: string;
+  children: React.ReactNode;
 }
 interface TextComponentProps {
-	texts: string;
-	inner?: boolean;
-	className?: string;
+  texts: string;
+  inner?: boolean;
+  className?: string;
 }
 
 const TextWrapper = ({ children }: TextWrapperProps) => {
-	const text = useRef<HTMLDivElement>(null);
-	const [scrollClass, setScrollClass] = useState<string>(
-		"opacity-0 text-white font-light"
-	);
+  const text = useRef<HTMLDivElement>(null);
+  const [scrollClass, setScrollClass] = useState<string>(
+    "opacity-0 text-white font-light"
+  );
 
-	const { scrollYProgress } = useScroll({
-		target: text,
-		offset:
-			window.innerWidth <= 768
-				? ["start end", "end start"]
-				: ["end end", "start start"],
-	});
+  const { scrollYProgress } = useScroll({
+    target: text,
+    offset:
+      window.innerWidth <= 768
+        ? ["start end", "end start"]
+        : ["end end", "start start"],
+  });
 
-	useEffect(() => {
-		const unsubscribe = scrollYProgress.onChange((latest) => {
-			if (latest < 0.1) {
-				setScrollClass("opacity-0 text-white font-light");
-			} else if (latest < 0.2) {
-				setScrollClass("opacity-50 text-[#8b939c] font-normal");
-			} else {
-				setScrollClass("opacity-100 text-[--third] font-medium");
-			}
-		});
-		return () => unsubscribe();
-	}, [scrollYProgress]);
+  useEffect(() => {
+    const unsubscribe = scrollYProgress.onChange((latest) => {
+      if (latest < 0.1) {
+        setScrollClass("opacity-0 text-white font-light");
+      } else if (latest < 0.2) {
+        setScrollClass("opacity-50 text-[#8b939c] font-normal");
+      } else {
+        setScrollClass("opacity-100 text-[--third] font-medium");
+      }
+    });
+    return () => unsubscribe();
+  }, [scrollYProgress]);
 
-	return (
-		<div ref={text}>
-			<motion.p className={`Raleway ${scrollClass} transition-500 =`}>
-				{children}
-			</motion.p>
-		</div>
-	);
+  return (
+    <div ref={text}>
+      <motion.p className={`Raleway ${scrollClass} transition-500 =`}>
+        {children}
+      </motion.p>
+    </div>
+  );
 };
 
 export const TextComponent = ({
-	className = "",
-	texts,
-	inner = false,
+  className = "",
+  texts,
+  inner = false,
 }: TextComponentProps) => {
-	const textList = createGroups(texts.split(" "));
+  const textList = createGroups(texts.split(" "));
 
-	return (
-		<section
-			id="textscroller"
-			className={`h-screen w-screen flex flex-col justify-center items-center text-2xl md:text-5xl gap-4 ${
-				inner ? "" : className
-			}`}
-		>
-			{textList.map((text, index) => (
-				<TextWrapper key={index} className={inner ? className : ""}>
-					{text}
-				</TextWrapper>
-			))}
-		</section>
-	);
+  return (
+    <section
+      id="textscroller"
+      className={`h-screen w-screen flex flex-col justify-center items-center text-2xl md:text-5xl gap-4 ${
+        inner ? "" : className
+      }`}
+    >
+      {textList.map((text, index) => (
+        <TextWrapper key={index} className={inner ? className : ""}>
+          {text}
+        </TextWrapper>
+      ))}
+    </section>
+  );
 };
