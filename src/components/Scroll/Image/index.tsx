f7bd89027f805cea744701c7e96071e4ec553cf8
unknown <tech2@imperio.com>
2024-07-03 17:59:14 +0530

    photos

--- src/components/Scroll/Image/index.tsx
+++ src/components/Scroll/Image/index.tsx
@@ -1,6 +1,6 @@
 import { useScroll, useTransform, motion } from "framer-motion";
 import { useRef } from "react";
-import { bigImg, setImg1, setImg2 } from "../../../assets/Images";
+import { bigImg, setImg1, setImg2, setImg3 } from "../../../assets/Images";
 import { ImageScrollProps, ImageScrollsProps } from "../../../types";
 
 const ImageScroll: React.FC<ImageScrollProps> = ({
@@ -39,20 +39,24 @@ const cards = [
   { img: bigImg, alt: "main image for scroll" },
   { img: setImg1, alt: "main image for scroll" },
   { img: setImg2, alt: "main image for scroll" },
-  { img: setImg2, alt: "main image for scroll" },
-  { img: setImg1, alt: "main image for scroll" },
-  { img: bigImg, alt: "main image for scroll" },
+  { img: setImg3, alt: "main image for scroll" },
 ];
 
 export const ImageScrolls: React.FC<ImageScrollsProps> = ({
   className = "",
 }) => {
   return (
-    <ImageScroll direction={-3000} className={className}>
+    <ImageScroll direction={-1000} className={className}>
       <div className="flex gap-4" style={{ right: 0 }}>
         {cards.map((card, index) => {
           return (
-            <img src={card.img} alt={card.alt} key={index} title={card.alt} />
+            <img
+              src={card.img}
+              alt={card.alt}
+              key={index}
+              title={card.alt}
+              className="max-h-[34rem] rounded-4xl w-auto"
+            />
           );
         })}
       </div>
