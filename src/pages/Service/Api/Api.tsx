bf504ea11b83761c1cd396bc4f17c055d34af293
Tech3 <Tech3@imperiorailing.com>
2024-11-27 12:07:10 +0530

    final changes

--- src/pages/Service/Api/Api.tsx
+++ src/pages/Service/Api/Api.tsx
@@ -1,3 +1,4 @@
+// use only localhost testing
 // export const BASE_URL = "http://localhost:3001";
 
 export const BASE_URL: string = "https://imperiobackend.onrender.com";
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //use server only localhost testing
   // server: {
   //   proxy: {
   //     "/dealerships": "http://localhost:3001",
-  //     "/product/dealerregistration": "http://localhost:3001",
-  //     "/product/dealerlogin": "http://localhost:3001",
+  //     "/dealerregistration": "http://localhost:3001",
+  //     "/dealerlogin": "http://localhost:3001",
   //   },
   // },
 });
